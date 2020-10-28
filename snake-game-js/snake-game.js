189c3200cbb2f66e115630dbcf340d87e0d8fa01
PaulaCBS <58895275+PaulaCBS@users.noreply.github.com>
2020-10-28 14:28:14 -0300

    adicionada cobrinha e css ao snake-game

--- snake-game-js/snake-game.js
+++ snake-game-js/snake-game.js
@@ -4,9 +4,24 @@ const box = 32;
 
 const createBG = () => {
     //cor
-    context.fillStyle = "lightGreen";
-    // eixo x, y e altura, largura (32 pixels)
+    context.fillStyle = "black";
+    // eixo x, y e altura, largura (16 quadrados de 32 pixels)
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
-
 createBG();
+
+let position = [
+    //posição da parte 1
+    {
+        x: 8 * box,
+        y: 8 * box
+    }
+];
+const createSnake = () => {
+    //adiciona cada parte da cobrinha, nas respectivas posições do array
+    for(i=0; i < position.length; i++){
+        context.fillStyle = "lightGreen";
+        context.fillRect(position[i].x, position[i].y, box, box);
+    }
+}
+createSnake();
